4688513b998de3820d7e1bac342a16cd4ca6ce98
Your Name <you@example.com>
2020-05-26 16:01:11 +0300

    Updates

---
+++ precache-manifest.eacd34e45cfa1c03b620fd7b8db1502c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8856ba8faaeaeb1a0c77e8f9cd7c5f8",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "a634ad30619b2bd6cd7b",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.108d8733.chunk.css"
+  },
+  {
+    "revision": "af76bbafa7b9333e5ce6",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.5fb0dc8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.5fb0dc8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a634ad30619b2bd6cd7b",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.7c1f8676.chunk.js"
+  },
+  {
+    "revision": "c3967852f518bc85728e",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.402d57cd.js"
+  }
+]);
