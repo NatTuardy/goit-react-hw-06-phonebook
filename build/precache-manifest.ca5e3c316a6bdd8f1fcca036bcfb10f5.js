8afec9f2f15975f699d7321fc67fbd90cd836c2b
Your Name <you@example.com>
2020-05-26 15:24:39 +0300

    added

---
+++ build/precache-manifest.ca5e3c316a6bdd8f1fcca036bcfb10f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6785d5229e955ba002d68f895a88071",
+    "url": "/react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "7ef93f8184f2d10146fb",
+    "url": "/react-hw-06-phonebook/static/css/main.108d8733.chunk.css"
+  },
+  {
+    "revision": "af76bbafa7b9333e5ce6",
+    "url": "/react-hw-06-phonebook/static/js/2.5fb0dc8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-06-phonebook/static/js/2.5fb0dc8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef93f8184f2d10146fb",
+    "url": "/react-hw-06-phonebook/static/js/main.79f540d8.chunk.js"
+  },
+  {
+    "revision": "0d7d7566cbc5a6d77d04",
+    "url": "/react-hw-06-phonebook/static/js/runtime-main.de0fd5c6.js"
+  }
+]);
